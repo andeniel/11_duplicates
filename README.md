5d8bfc5241a741b682dc8753f0e199800b4c7353
Timur Jaganov <andeniel@gmail.com>
2017-08-29 15:30:32 +0600

    completed task

--- README.md
+++ README.md
@@ -1,7 +1,47 @@
-# Anti-Duplicator
+# Поиск дубликатов
 
-[TODO. There will be project description]
+Скрипт позволяет найти всех дублирующие файлы в указанной папке и всех подпапках внутри и выдает список найденных файлов.
 
-# Project Goals
+# Как запустить
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+
+Запуск на Linux:
+
+```#!bash
+# Программа принимает 1 параметр
+# python3 duplicates.py -f [путь к папке]
+# -f [указываете путь к папке]
+
+$ python3 duplicates.py -f ./test_folders
+# пример ответа скрипта
+Сканирование папок -> ./test_folders
+Сканирование папок -> ./test_folders/wallpaper
+Сканирование папок -> ./test_folders/wallpaper/nextfolder
+Сканирование папок -> ./test_folders/Teambuilding_21_07_2017
+
+Дубликаты найдены:
+Эти файлы одинаковые. Названия файлов могут быть разными, но содержание одинаковое:
+
+--- одинаковые файлы ---
+./test_folders/wallpaper/5.jpg
+./test_folders/wallpaper/nextfolder/new5.jpg
+./test_folders/Teambuilding_21_07_2017/5.jpg
+
+
+--- одинаковые файлы ---
+./test_folders/wallpaper/nextfolder/Image_from_Skype.jpg
+./test_folders/Teambuilding_21_07_2017/Image_from_Skype.jpg
+
+
+--- одинаковые файлы ---
+./test_folders/whats.jpg
+./test_folders/Teambuilding_21_07_2017/4.jpg
+
+```
+
+Запуск на Windows происходит аналогично.
+
+# Цели проекта
+
+Код создан в учебных целях. В рамках учебного курса по веб-разработке ― [DEVMAN.org](https://devman.org)
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,54 @@
+import argparse
+import sys
+import os
+import hashlib
+
+
+def hash_file(file_path, block_size=65536):
+    with open(file_path, 'rb') as file_data:
+        hasher = hashlib.md5()
+        buffer = file_data.read(block_size)
+        while buffer:
+            hasher.update(buffer)
+            buffer = file_data.read(block_size)
+        return hasher.hexdigest()
+
+
+def print_duplicates(duplicates):
+    clear_dupl = list(filter(lambda x: len(x) > 1, duplicates.values()))
+
+    if clear_dupl:
+        print("\nДубликаты найдены:")
+        print(
+            "Эти файлы одинаковые. Названия файлов могут быть разными,"
+            " но содержание одинаковое:\n")
+        for duplicate in clear_dupl:
+            print("--- одинаковые файлы ---")
+            for file_path in duplicate:
+                print(file_path)
+            print("\n")
+    else:
+        print("\nДубликаты не найдены.")
+
+
+def get_duplicates(folder_path):
+    duplicates = {}
+    for dir_name, _, file_list in os.walk(folder_path):
+        for file_name in file_list:
+            file_path = os.path.join(dir_name, file_name)
+            file_hash = hash_file(file_path)
+            if file_hash in duplicates:
+                duplicates[file_hash].append(file_path)
+            else:
+                duplicates[file_hash] = [file_path]
+
+    return duplicates
+
 
 if __name__ == '__main__':
-    pass
+    aparser = argparse.ArgumentParser()
+    aparser.add_argument(
+        "-f", "--folder", required=True, help="Путь к папке")
+    args = vars(aparser.parse_args())
+    duplicates = get_duplicates(args['folder'])
+    print_duplicates(duplicates)
